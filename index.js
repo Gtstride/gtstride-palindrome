7169978c777550f3b47a0db9c33132ac8d181480
Godstime <lewisagho47@gmail.com>
2019-04-18 14:14:13 +0100

    Finish working and refactored palindrome method

--- index.js
+++ index.js
@@ -33,22 +33,44 @@
 
 
 module.exports = Phrase;
+
 // Adds `reverse` to all strings.
 String.prototype.reverse = function() {
-    return Array.from(this).reverse().join("");
+  return Array.from(this).reverse().join("");
+}
+
+// Defines a Phrase object.
+function Phrase(content) {
+  this.content = content;
+
+  // Returns content processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.letters().toLowerCase();
   }
-  
-  // Defines a Phrase object.
-  function Phrase(content) {
-    this.content = content;
-  
-    // Returns content processed for palindrome testing.
-    this.processedContent = function processedContent() {
-      return this.content.toLowerCase();
-    }
-  
-    // Returns true if the phrase is a palindrome, false otherwise.
-    this.palindrome = function palindrome() {
-      return this.processedContent() === this.processedContent().reverse();
-    }
+
+  // Returns the letters in the content.
+  // For example:
+  //   new Phrase("Hello, world!").letters() === "Helloworld"
+  this.letters = function letters() {
+    // let theLetters = [];
+    // const letterRegex = /[a-z]/i;
+    // Array.from(this.content).forEach(function(character) {
+    //   if (character.match(letterRegex)) {
+    //     theLetters.push(character);
+    //   }
+    // });
+    // for (let i = 0; i < this.content.length; i++) {
+    //   let character = this.content.charAt(i);
+    //   if (character.match(/[a-zA-Z]/)) {
+    //     theLetters.push(character);
+    //   }
+    // }
+    //return theLetters.join("");
+    return Array.from(this.content).filter(c => c.match(/[a-z]/i)).join("");
   }
+
+  // Returns true if the phrase is a palindrome, false otherwise.
+  this.palindrome = function palindrome() {
+    return this.processedContent() === this.processedContent().reverse();
+  }
+}
